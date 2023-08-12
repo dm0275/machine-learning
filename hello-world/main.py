99ab3433ee76922bb620b6e69a20c6a45ca8f090
Daniel Mancilla <dm0275@gmail.com>
2023-08-11 19:56:00 -0500

    Updated project

--- hello-world/main.py
+++ hello-world/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-from tensorflow import keras
+import tensorflow as tf
 
 # Load the trained model
-loaded_model = keras.models.load_model('x_y_model.h5')
+loaded_model = tf.keras.models.load_model('x_y_model.h5')
 
 print(loaded_model.predict([4.0]))
--- hello-world/train.py
+++ hello-world/train.py
@@ -2,18 +2,18 @@
 
 import tensorflow as tf
 import numpy as np
-from tensorflow import keras
 
 # Define input (x) and output (y) data
 input_x = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 output_y = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
 
 # Build a sequential model
-model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
-model.compile(optimizer='sgd', loss='mean_squared_error')
+model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
+model.compile(optimizer=tf.keras.optimizers.legacy.SGD(), loss='mean_squared_error')
 
 # Train the model
-model.fit(input_x, output_y, epochs=100)
+model.fit(input_x, output_y, epochs=50)
 
 # Save the model
-model.save('x_y_model.h5')
+model.save('x_y_model.h5')  # Unable to load the model w/ a '.keras' extension (after saving) due to this issue:
+# https://github.com/keras-team/keras/issues/18278
