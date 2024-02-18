70023d4f34f1c9da19dfd09c5b91ffba105e1401
Daniel Mancilla <dm0275@gmail.com>
2024-02-17 18:00:31 -0600

    Added linear regresssion project

---
+++ linear-regression/train.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def compute_model_output(x, w, b):
+    """
+    Computes the prediction of a linear model
+    Args:
+      x (ndarray (m,)): Data, m examples
+      w,b (scalar)    : model parameters
+    Returns
+      f_wb (ndarray (m,)): model prediction
+    """
+    m = x.shape[0]
+    f_wb = np.zeros(m)
+    for i in range(m):
+        f_wb[i] = w * x[i] + b
+
+    return f_wb
+
+# x_train is the input variable (size in 1000 square feet)
+# y_train is the target (price in 1000s of dollars)
+x_train = np.array([1.0, 2.0])
+y_train = np.array([300.0, 500.0])
+print(f"x_train = {x_train}")
+print(f"y_train = {y_train}")
+
+# m is the number of training examples
+m = len(x_train)
+print(f"Number of training examples is: {m}")
+
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r')
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.show()
+
+w = 200
+b = 100
+print(f"w: {w}")
+print(f"b: {b}")
+
+tmp_f_wb = compute_model_output(x_train, w, b,)
+
+# Plot our model prediction
+plt.plot(x_train, tmp_f_wb, c='b',label='Our Prediction')
+
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r',label='Actual Values')
+
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.legend()
+plt.show()
