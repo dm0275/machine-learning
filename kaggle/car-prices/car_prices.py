4070fcf171f131b93aadb889d8889325768d9c22
Daniel Mancilla <dm0275@gmail.com>
2024-03-24 13:20:59 -0500

    Added graphs for car_prices

--- kaggle/car-prices/car_prices.py
+++ kaggle/car-prices/car_prices.py
@@ -1,34 +1,15 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import numpy as np
-import seaborn as sns
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Import the data set
 data_frame = pd.read_csv("car_prices.csv")
 
-# X_train = pd.DataFrame(data_frame.loc[:, data_frame.columns != 'sellingprice']).to_numpy()
 
 X_train = pd.Series(data_frame.year)
-
 y_train = pd.Series(data_frame.sellingprice)
-features = ['size(sqft)','bedrooms','floors','age']
 
-# print(y_train[0])
-# print(X_train[0])
-# print(type(X_train))
-# print(y_train.to_frame())
-# print(data_frame.columns)
-
-# fig,ax=plt.subplots(1, 4, figsize=(12, 3), sharey=True)
-# for i in range(len(ax)):
-#     ax[i].scatter(X_train[:,i],y_train)
-#     ax[i].set_xlabel(features[i])
-# ax[0].set_ylabel("Price (1000's)")
-# plt.show()
 
 plt.scatter(X_train, y_train)
 plt.title("Car Price by Year")
@@ -44,29 +25,14 @@ plt.xlabel("Mileage")
 plt.ylabel("Car Price")
 plt.show()
 
+
+# Count the number of car sales per make
+plt.figure(figsize=(20,40))
 X_train = pd.Series(data_frame.make)
 
-# plt.bar(y_train, X_train)
-# plt.tight_layout()
-# plt.show()
-
-first_10_rows = pd.DataFrame(data_frame.iloc[:20])
-print(first_10_rows)
-
-name = data_frame['make']#.head(20)
-price = data_frame['sellingprice']#.head(20)
-
-# Figure Size
-fig = plt.figure(figsize =(10, 7))
-
-# Horizontal Bar Plot
-# plt.bar(name, price)
-plt.bar(name[0:20], price[0:20])
-
+X_train.value_counts().plot(kind='bar', width=0.4)
+plt.title('Number of Cars by Make')
+plt.xlabel('Make')
+plt.ylabel('Count')
+plt.xticks(rotation=90)
 plt.show()
-
-print(name[0:20])
-
-# ax = first_10_rows.plot.bar(x="make", y="sellingprice", rot=0)
-# plt.tight_layout()
-# plt.show()
